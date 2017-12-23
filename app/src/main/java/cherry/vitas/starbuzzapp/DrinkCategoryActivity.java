2004cfb6a5299b1f84b031d2e9828effc2434868
vitascherry <vitas.cherry@gmail.com>
2017-12-23 02:35:58 +0200

    Added clickable favorites

--- app/src/main/java/cherry/vitas/starbuzzapp/DrinkCategoryActivity.java
+++ app/src/main/java/cherry/vitas/starbuzzapp/DrinkCategoryActivity.java
@@ -2,24 +2,46 @@ package cherry.vitas.starbuzzapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-import cherry.vitas.starbuzzapp.entities.Drink;
+import cherry.vitas.starbuzzapp.entities.StarbuzzDatabaseHelper;
 
 public class DrinkCategoryActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_list_item_1, Drink.drinks);
-        ListView listDrinks = findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         //Create the listener
         AdapterView.OnItemClickListener itemClickListener =
@@ -39,5 +61,12 @@ public class DrinkCategoryActivity extends Activity {
 
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
